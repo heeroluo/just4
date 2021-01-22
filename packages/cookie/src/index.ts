6744fcd5d11384615e21c72fecadff7072ce5278
Mr.Luo <heeroluo@126.com>
2021-01-22 17:42:46 +0800

    补充代码调用示例

--- packages/cookie/src/index.ts
+++ packages/cookie/src/index.ts
@@ -11,6 +11,11 @@ import { ICookieGetterOptions, ICookieSetterOptions } from './interfaces';
 
 /**
  * 读取 cookie。
+ * @example
+ * ```typescript
+ * import { getCookie } from '@just4/cookie/index';
+ * getCookie('test');
+ * ```
  * @param key cookie 名。
  * @param options 选项。
  * @returns cookie 值（cookie 不存在时返回空字符串）。
@@ -36,6 +41,15 @@ export function getCookie(key: string, options?: ICookieGetterOptions): string {
 
 /**
  * 写入 cookie。
+ * @example
+ * ```typescript
+ * import { setCookie } from '@just4/cookie/index';
+ * setCookie('test', 1, {
+ *   domain: 'abc.com',
+ *   path: '/',
+ *   expires: '30days'
+ * });
+ * ```
  * @param key cookie 名。
  * @param value cookie 值。
  * @param options 选项。
@@ -76,6 +90,11 @@ const shouldSetEmptyBeforeRemove: boolean = (function() {
 
 /**
  * 移除 cookie。
+ * @example
+ * ```typescript
+ * import { removeCookie } from '@just4/cookie/index';
+ * removeCookie('test');
+ * ```
  * @param key cookie 名。
  * @param options 选项。
  */
