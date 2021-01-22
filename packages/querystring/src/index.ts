efde46087104ad5484272dc235a6dadb66592e15
Mr.Luo <heeroluo@126.com>
2021-01-22 18:15:20 +0800

    补充代码调用示例

--- packages/querystring/src/index.ts
+++ packages/querystring/src/index.ts
@@ -9,6 +9,12 @@ import { IQSParseOptions, IQSStringifyOptions } from './interfaces';
 
 /**
  * 把查询字符串解析为键值对集合。
+ * @example
+ * ```typescript
+ * import { parse } from '@just4/querystring/index';
+ * parse('id=0&str=hello'); // { id: '0', str: 'hello' }
+ * parse('id=0&id=1'); // { id: ['0', '1'] }
+ * ```
  * @param str 查询字符串。
  * @param options 解析选项。
  * @returns 键值对集合。
@@ -50,6 +56,12 @@ export function parse(
 
 /**
  * 把键值对序列化为查询字符串。
+ * @example
+ * ```typescript
+ * import { stringify } from '@just4/querystring/index';
+ * stringify({ id: '0', str: 'hello' }); // 'id=0&str=hello'
+ * stringify({ id: ['0', '1'] }); // 'id=0&id=1'
+ * ```
  * @param data 键值对。
  * @param options 序列化选项。
  * @returns 序列化结果。
@@ -96,6 +108,14 @@ export function stringify(
 
 /**
  * 把键值对序列化为查询字符串后拼接到指定 URL。
+ * @example
+ * ```typescript
+ * import { appendToURL } from '@just4/querystring/index';
+ * appendToURL('https://abc.com/?a=1#hash', {
+ *   b: 2,
+ *   c: 3
+ * }); // https://abc.com/?a=1&b=2&c=3#hash
+ * ```
  * @param url 指定URL。
  * @param data 键值对。
  * @param options 序列化选项。
