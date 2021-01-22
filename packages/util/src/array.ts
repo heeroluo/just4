3ab75f389790730b30299d5b2aa692aee0817edb
Mr.Luo <heeroluo@126.com>
2021-01-22 16:23:45 +0800

    补充代码调用示例

--- packages/util/src/array.ts
+++ packages/util/src/array.ts
@@ -5,6 +5,11 @@
 
 /**
  * 把类数组转换为数组。
+ * @example
+ * ```typescript
+ * import { toArray } from '@just4/util/array';
+ * toArray(document.getElementsByTagName('div'));
+ * ```
  * @param obj 类数组。
  * @returns 新数组。
  */
@@ -24,6 +29,11 @@ export function toArray<T>(obj: ArrayLike<T>): Array<T> {
 
 /**
  * 把源数组的元素合并到目标数组。
+ * @example
+ * ```typescript
+ * import { mergeArray } from '@just4/util/array';
+ * mergeArray([1, 2, 3], [7, 9, 10]); // [1, 2, 3, 7, 9, 10]
+ * ```
  * @param target 目标数组。
  * @param source 源数组。
  * @returns 目标数组。
@@ -42,6 +52,11 @@ export function mergeArray<T>(target: Array<T>, source?: ArrayLike<T>): Array<T>
 
 /**
  * 检查指定对象是否为类数组结构。
+ * @example
+ * ```typescript
+ * import { isArrayLike } from '@just4/util/array';
+ * isArrayLike(document.getElementsByTagName('div')); // true
+ * ```
  * @param obj 指定对象。
  * @returns 指定对象是否为类数组结构。
  */
--- packages/util/src/type.ts
+++ packages/util/src/type.ts
@@ -7,6 +7,11 @@ const toString = Object.prototype.toString;
 
 /**
  * 检查变量是否 Function 类型。
+ * @example
+ * ```typescript
+ * import { isFunction } from '@just4/util/type';
+ * isFunction(() => { alert('a'); }); // true
+ * ```
  * @param value 待测变量。
  * @returns `value` 是否 Function 类型。
  */
@@ -16,6 +21,11 @@ export function isFunction(value: unknown): boolean {
 
 /**
  * 检查变量是否 Date 类型。
+ * @example
+ * ```typescript
+ * import { isDate } from '@just4/util/type';
+ * isDate(new Date); // true
+ * ```
  * @param value 待测变量。
  * @returns `value` 是否 Date 类型。
  */
@@ -25,6 +35,12 @@ export function isDate(value: unknown): boolean {
 
 /**
  * 检查变量是否 Object 类型。
+ * @example
+ * ```typescript
+ * import { isObject } from '@just4/util/type';
+ * isObject({}); // true
+ * isObject(new Date); // false
+ * ```
  * @param value 待测变量。
  * @returns `value` 是否 Object 类型。
  */
