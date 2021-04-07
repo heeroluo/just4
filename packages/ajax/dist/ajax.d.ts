e35abc097e3e6e6d219ce2200e1af5a4620865c4
Mr.Luo <heeroluo@126.com>
2021-04-07 11:08:14 +0800

    完善 README，修改版本号

--- packages/ajax/dist/ajax.d.ts
+++ packages/ajax/dist/ajax.d.ts
@@ -6,6 +6,30 @@ import { cancelRequest } from './internal/ajax-record';
 import { IAJAXOptions, IAJAXResponse } from './interfaces';
 /**
  * 发送 AJAX 请求。
+ * @example
+ * ```typescript
+ * import { send } from '@just4/ajax/ajax';
+ * const { data } = await send('/api/ajax/json', {
+ *   method: 'get',
+ *   params: { num: 100 }
+ * });
+ * const { data } = await send('/api/ajax/json', {
+ *   method: 'post',
+ *   data: { num: 100 }
+ * });
+ * ```
+ * @example
+ * ```typescript
+ * import { send, cancel } from '@just4/ajax/ajax';
+ * let ajaxId = 0;
+ * send('/api/ajax/timeout', {
+ *  receiveCancelId: function(id) { ajaxId = id; }
+ * });
+ * // 2s 后中断请求
+ * setTimeout(function() {
+ *   cancel(ajaxId);
+ * }, 2000);
+ * ```
  * @param url 请求 URL。
  * @param options 请求选项。
  * @returns 发送请求的 promise 实例。
