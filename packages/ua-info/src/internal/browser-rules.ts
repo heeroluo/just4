f1fc16c2e3312303f5b4c438b2c6248ee89ac5a6
Mr.Luo <heeroluo@126.com>
2021-08-04 10:49:49 +0800

    修正 Edge(EdgeHTML) 识别规则

--- packages/ua-info/src/internal/browser-rules.ts
+++ packages/ua-info/src/internal/browser-rules.ts
@@ -11,9 +11,9 @@ import { IRule } from './types';
  * 浏览器内核匹配规则。
  */
 export const browserRules: IRule[] = [
+  { name: 'edge', regExp: /\bEdge\/([\d.]+)/ },
   { name: 'chrome', regExp: /\b(?:Chrome|CrMo|CriOS)\/([\d.]+)/ },
   { name: 'safari', regExp: /\b(?:Version\/([\d.]+).*\s?)?Safari\b/ },
-  { name: 'edge', regExp: /\bEdge?\/([\d.]+)/ },
   { name: 'ie', regExp: /\bMSIE\s(\d+)/i },
   { name: 'ie', regExp: /\bTrident\/.*;\srv:(\d+)/ },
   { name: 'firefox', regExp: /\bFirefox\/([\d.]+)/ },
