53eef671cd0a3e2662a3f3a80eb5b6c8c1934726
Mr.Luo <heeroluo@126.com>
2021-08-27 00:52:27 +0800

    生成文档

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -17,22 +17,26 @@ if (typeof window !== 'undefined') {
 }
 
 /**
- * sessionStorage 存取方法（通过 StorageWrap 包装了 sessionStorage）。
+ * sessionStorage 的封装（通过 StorageWrap 包装了 sessionStorage）。
  * @example
+ * ``` javascript
  * const user = { name: 'Tom', pet: 'cat' };
  * session.setAsJSON('user', user);
  * typeof session.get('user'); // 'string'
  * typeof session.getAsJSON('user'); // 'object'
+ * ```
  */
 export const session = new StorageWrap(sessionStorage);
 
 /**
- * localStorage 存取方法（通过 StorageWrap 包装了 localStorage）。
+ * localStorage 的封装（通过 StorageWrap 包装了 localStorage）。
  * @example
+ * ``` javascript
  * const user = { name: 'Tom', pet: 'cat' };
  * local.setAsJSON('user', user);
  * local.getAsJSON('user');
  * typeof local.get('user'); // 'string'
  * typeof local.getAsJSON('user'); // 'object'
+ * ```
  */
 export const local = new StorageWrap(localStorage);
