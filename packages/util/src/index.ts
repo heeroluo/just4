6170a6d52a8fc5138339a654d6ae1f6209a8312c
Mr.Luo <heeroluo@126.com>
2021-09-06 15:28:17 +0800

    增加调用入口文件

---
+++ packages/util/src/index.ts
@@ -0,0 +1,22 @@
+/**
+ * 调用入口。
+ * @packageDocumentation
+ */
+
+export {
+  isFunction,
+  isDate,
+  isObject
+} from './type';
+
+export {
+  hasOwnProp,
+  assignProps,
+  isEmpty
+} from './object';
+
+export {
+  toArray,
+  mergeArray,
+  isArrayLike
+} from './array';
