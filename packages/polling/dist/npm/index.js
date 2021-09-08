358b17f57093ccad2f2e158ab8e1bc13430b004d
Mr.Luo <heeroluo@126.com>
2021-09-08 14:55:36 +0800

    重新构建

--- packages/polling/dist/npm/index.js
+++ packages/polling/dist/npm/index.js
@@ -1,5 +1,3 @@
-import { assignProps } from "@just4/util/object";
-
 let theGlobal;
 
 if (typeof window !== "undefined") {
@@ -8,16 +6,31 @@ if (typeof window !== "undefined") {
     theGlobal = global;
 }
 
+const pollingTasks = [];
+
 export class Polling {
     constructor(executor, options) {
+        this._options = {
+            interval: 1e3,
+            breakOnError: false
+        };
         this._started = false;
         this._isExecuting = false;
         this._shouldImmediate = false;
         this._executor = executor;
-        this._options = assignProps({
-            interval: 1e3,
-            breakOnError: false
-        }, options);
+        this.updateOptions(options);
+    }
+    static stopAll() {
+        for (let i = pollingTasks.length - 1; i >= 0; i--) {
+            pollingTasks[i].stop();
+        }
+    }
+    updateOptions(options) {
+        var _a, _b;
+        if (options) {
+            this._options.interval = (_a = options.interval) !== null && _a !== void 0 ? _a : this._options.interval;
+            this._options.breakOnError = (_b = options.breakOnError) !== null && _b !== void 0 ? _b : this._options.breakOnError;
+        }
     }
     _exec() {
         let result;
@@ -26,20 +39,24 @@ export class Polling {
         } catch (e) {
             if (this._options.breakOnError) {
                 this.stop();
+            } else {
+                this._next();
             }
+            throw e;
         }
         if (result && typeof result.then === "function") {
             this._isExecuting = true;
             result.then((() => {
                 this._isExecuting = false;
                 this._next();
-            }), (() => {
+            }), (e => {
                 this._isExecuting = false;
                 if (this._options.breakOnError) {
                     this.stop();
                 } else {
                     this._next();
                 }
+                throw e;
             }));
         } else {
             this._next();
@@ -75,11 +92,21 @@ export class Polling {
         }
     }
     start() {
+        if (this._started) {
+            return;
+        }
         this._started = true;
+        pollingTasks.push(this);
         this._exec();
     }
     stop() {
         this._clearTimeout();
         this._started = false;
+        for (let i = pollingTasks.length - 1; i >= 0; i--) {
+            if (pollingTasks[i] === this) {
+                pollingTasks.splice(i, 1);
+                break;
+            }
+        }
     }
 }
