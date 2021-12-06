23e73acb9cd980813ef4da147aa740bcadf12168
Mr.Luo <heeroluo@126.com>
2021-12-06 11:18:28 +0800

    重新构建

--- packages/util/dist/es/index.js
+++ packages/util/dist/es/index.js
@@ -1,5 +1,1 @@
-export { isFunction, isDate, isObject } from "./type";
-
-export { hasOwnProp, assignProps, isEmpty } from "./object";
-
-export { toArray, mergeArray, isArrayLike } from "./array";
+export{isFunction,isDate,isObject}from"./type";export{hasOwnProp,assignProps,isEmpty}from"./object";export{toArray,mergeArray,isArrayLike}from"./array";
