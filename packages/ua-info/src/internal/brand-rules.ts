8472edb58ad7540958f04ad03d6583560b7b5fd5
Mr.Luo <heeroluo@126.com>
2022-11-15 16:39:48 +0800

    更新品牌特征库

--- packages/ua-info/src/internal/brand-rules.ts
+++ packages/ua-info/src/internal/brand-rules.ts
@@ -24,15 +24,17 @@ export const androidRules: IRule[] = [
   { name: 'huawei', regExp: /\b(?:huawei|honor)/i },
   { name: 'vivo', keywords: ['vivo'] },
   { name: 'oppo', keywords: ['oppo'] },
-  { name: 'mi', keywords: ['redmi', 'hongmi', 'xiaomi', 'shark'] },
+  { name: 'mi', keywords: ['redmi', 'hongmi', 'xiaomi', 'XiaoMi', 'shark', 'Mi'] },
   { name: 'samsung', keywords: ['samsung', 'galaxy'] },
   { name: 'oneplus', keywords: ['oneplus', 'one'] },
 
   // 型号规则
   { name: 'huawei', modelRegExp: /^Mate\s\d{2}/ },
   { name: 'huawei', modelRegExp: /^Nova\s\d$/ },
-  { name: 'huawei', modelRegExp: /^(?:CHE|CHM|Che1|VIE|BND|PAR|JKM|EML|OXF|VOG|JSN|VCE|STK|STF|BZT|YAL|INE|COR|SPN|AGS2|MAR|LYA|BKL|CLT|SEA|MHA|EVR|VKY|ANE|ALP|TIT|MRX|ELE|DVC|SCM|JLH|JDN2|JEF|Hera|Hebe)-/ },
+  { name: 'huawei', modelRegExp: /^(?:CHE|CHM|Che1|VIE|BND|PAR|JKM|EML|OXF|VOG|JSN|VCE|STK|STF|BZT|YAL|INE|COR|SPN|AGS2|MAR|LYA|BKL|CLT|SEA|MHA|EVR|VKY|ANE|ALP|TIT|MRX|ELE|DVC|SCM|JLH|JDN2|JEF|Hera|Hebe|FIO|ELZ|MNT|LRA)-/ },
   { name: 'huawei', modelRegExp: /^TYH\d+[A-Z]$/ },
+  { name: 'huawei', regExp: /\bLiantongVP\d{3}\b/ },
+  { name: 'huawei', regExp: /\bCMDCSP\d{3}\b/ },
   { name: 'mi', modelRegExp: /^MI\s?(?:\d|CC|Note|MAX|PLAY|PAD)/i },
   { name: 'mi', modelRegExp: /^MIX\s\dS?/ },
   { name: 'mi', modelRegExp: /^(?:AWM|SKR|SKW|DLT)-/ },
@@ -42,5 +44,6 @@ export const androidRules: IRule[] = [
   { name: 'samsung', modelRegExp: /^SC-\d{2}[A-Z]$/ },
   { name: 'samsung', modelRegExp: /^SH[WV]-/ },
   { name: 'samsung', modelRegExp: /^GT[-_][A-Z][A-Z0-9]{3,}$/i },
+  { name: 'oppo', modelRegExp: /^(?:CPH|OPD)\d{4}$/ },
   { name: 'oneplus', modelRegExp: /^(?:KB|HD|IN|GM|NE|LE|MT)\d{4}$/ }
 ];
