3f43b4c91d519d655b2519b7ce122890a6d12047
Mr.Luo <heeroluo@126.com>
2023-09-11 17:30:18 +0800

    优化代码实现

--- packages/querystring/src/parse.ts
+++ packages/querystring/src/parse.ts
@@ -1,5 +1,5 @@
 /**
- * 解析查询字符串。
+ * 提供解析查询字符串的方法。
  * @packageDocumentation
  */
 
@@ -9,31 +9,30 @@ import { IQSParseOptions } from './interfaces';
 
 /**
  * 把查询字符串解析为键值对集合。
+ * @param str 查询字符串。
+ * @param options 解析选项。
+ * @returns 键值对集合。
  * @example
  * ```javascript
  * import { parse } from '@just4/querystring';
  * parse('id=0&str=hello'); // { id: '0', str: 'hello' }
  * parse('id=0&id=1'); // { id: ['0', '1'] }
  * ```
- * @param str 查询字符串。
- * @param options 解析选项。
- * @returns 键值对集合。
  */
 export function parse(
   str: string, options?: Readonly<IQSParseOptions>
 ): { [key: string]: string | string[] } {
-  if (typeof str !== 'string') {
-    throw new Error('The str argument must be a string type');
-  }
+  const result: { [key: string]: string | string[] } = Object.create(null);
+
+  if (typeof str !== 'string') { return result; }
 
   const opts: IQSParseOptions = assignProps({
     decode: decodeURIComponent
   }, options);
 
-  const result: { [key: string]: string | string[] } = Object.create(null);
-
   str.split('&').forEach(function(pair) {
     if (!pair) { return; }
+
     const pairArr = pair.split('=');
     let key = pairArr[0];
     let value = pairArr[1] || '';
@@ -44,8 +43,13 @@ export function parse(
 
     if (hasOwnProp(result, key)) {
       // 出现重复 key 值时解析为数组
-      if (!Array.isArray(result[key])) { result[key] = [<string>result[key]]; }
-      (<string[]>result[key]).push(value);
+      const currentValue = result[key];
+      const values = Array.isArray(currentValue)
+        ? currentValue
+        : [currentValue];
+      values.push(value);
+      result[key] = values;
+
     } else {
       result[key] = value;
     }
--- packages/querystring/src/stringify.ts
+++ packages/querystring/src/stringify.ts
@@ -1,5 +1,5 @@
 /**
- * 序列化为查询字符串。
+ * 提供序列化为查询字符串的方法。
  * @packageDocumentation
  */
 
@@ -7,50 +7,69 @@ import { hasOwnProp, assignProps } from '@just4/util/index';
 import { IQSStringifyOptions } from './interfaces';
 
 
+// 把序列化结果插入到结果集中
+function addToResult(
+  result: string[],
+  key: string,
+  value: unknown,
+  opts: Readonly<IQSStringifyOptions>
+): void {
+  if (value == null) { value = ''; }
+  // 忽略空值的情况
+  if (value === '' && opts.ignoreEmpty) { return; }
+  if (typeof opts.encode === 'function') {
+    key = opts.encode(key);
+    const valueType = typeof value;
+    if (['boolean', 'string', 'number'].indexOf(valueType) !== -1) {
+      value = opts.encode(String(value));
+    } else {
+      value = '';
+    }
+  }
+  result.push(key + '=' + value);
+}
+
+// 处理一个参数有多个值的情况，
+function handleMultiValues(
+  result: string[],
+  key: string,
+  values: unknown[],
+  opts: Readonly<IQSStringifyOptions>
+): void {
+  values.forEach((item) => {
+    addToResult(result, key, item, opts);
+  });
+}
+
 /**
  * 把键值对序列化为查询字符串。
+ * @param data 键值对。
+ * @param options 序列化选项。
+ * @returns 序列化结果。
  * @example
  * ```javascript
  * import { stringify } from '@just4/querystring';
  * stringify({ id: '0', str: 'hello' }); // 'id=0&str=hello'
  * stringify({ id: ['0', '1'] }); // 'id=0&id=1'
  * ```
- * @param data 键值对。
- * @param options 序列化选项。
- * @returns 序列化结果。
  */
 export function stringify(
-  data: object,
-  options?: Readonly<IQSStringifyOptions>
+  data: any, options?: Readonly<IQSStringifyOptions>
 ): string {
   const opts: IQSStringifyOptions = assignProps({
     encode: encodeURIComponent
   }, options);
 
   const result: string[] = [];
-  function addToResult(key: string, value: unknown): void {
-    if (value == null) { value = ''; }
-    // 忽略空值的情况
-    if (value === '' && opts.ignoreEmpty) { return; }
-    if (typeof opts.encode === 'function') {
-      key = opts.encode(key);
-      value = opts.encode(String(value));
-    }
-    result.push(key + '=' + value);
-  }
 
   let key: string, value: unknown;
-
-  // 避免在循环中生成匿名函数，提到循环外
-  function loopItem(item: unknown): void { addToResult(key, item); }
-
   for (key in data) {
     if (hasOwnProp(data, key)) {
-      value = (<any>data)[key];
+      value = data[key];
       if (Array.isArray(value)) {
-        value.forEach(loopItem);
+        handleMultiValues(result, key, value, opts);
       } else {
-        addToResult(key, value);
+        addToResult(result, key, value, opts);
       }
     }
   }
