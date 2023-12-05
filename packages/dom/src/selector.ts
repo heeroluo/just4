1c052ef0d4e6c2cbd02d792b71f72947f1726939
Mr.Luo <heeroluo@126.com>
2023-12-05 16:45:37 +0800

    元素有 id 时，不再使用临时 id

--- packages/dom/src/selector.ts
+++ packages/dom/src/selector.ts
@@ -6,7 +6,6 @@
 
 // 临时的 id 属性值，用于匹配 querySelectorAll 的上下文
 const TEMP_ID = 'just4__query__context';
-const TEMP_ID_SELECTOR = '#' + TEMP_ID + ' ';
 
 /**
  * 通过选择器和上下文匹配元素（querySelectorAll 的打补丁版）。
@@ -15,13 +14,13 @@ const TEMP_ID_SELECTOR = '#' + TEMP_ID + ' ';
  * @returns 匹配到的元素。
  */
 export function querySelectorAll(
-  selector: string, context: HTMLElement | HTMLDocument = document
+  selector: string, context: HTMLElement | Document = document
 ): ArrayLike<HTMLElement> {
   let ctxParent: HTMLElement | null;
 
   // context 是 document，相当于 document.querySelectorAll；
   // ctxParent 不存在时，无法使用后续的逻辑修正 querySelectorAll 的行为，
-  // 也直接使用 document.querySelectorAll
+  // 也直接使用 context.querySelectorAll
   if (context.nodeType === 9 || !(ctxParent = context.parentElement)) {
     return context.querySelectorAll<HTMLElement>(selector);
   }
@@ -33,23 +32,36 @@ export function querySelectorAll(
   // 仍然能够匹配到 span 元素，与 CSS 选择器匹配结果有差异，
   // 此处用于修复该问题，原理是通过 id 选择器限定查找范围
 
-  selector = TEMP_ID_SELECTOR + selector.replace(/\s*,\s*/g, function(match) {
-    return match + TEMP_ID_SELECTOR;
-  });
-
   const ctxElement = <HTMLElement>context;
 
-  // 把 id 临时换成 TEMP_ID 进行范围限定
-  const origId = ctxElement.getAttribute('id');
-  ctxElement.setAttribute('id', TEMP_ID);
+  let id = ctxElement.getAttribute('id');
+  let shouldRemoveId = 0;
+  // 元素本身没有 id 时，把 id 临时设成 TEMP_ID 进行范围限定
+  // 完成查询后要移除 id
+  if (!id) {
+    // 区分没有 id 属性和 id 属性值为空字符串的情况
+    shouldRemoveId = id == null ? 1 : 2;
+
+    id = TEMP_ID;
+    ctxElement.setAttribute('id', id);
+  }
+
+  const selectorPrefix = '#' + id + ' ';
+  selector = selectorPrefix + selector.replace(/\s*,\s*/g, function(match) {
+    return match + selectorPrefix;
+  });
+
   try {
     return ctxParent.querySelectorAll<HTMLElement>(selector);
   } finally {
-    // 无论如何，恢复原来的 id
-    if (origId != null) {
-      ctxElement.setAttribute('id', origId);
-    } else {
-      ctxElement.removeAttribute('id');
+    // 恢复原来的 id 属性
+    switch (shouldRemoveId) {
+      case 1:
+        ctxElement.removeAttribute('id');
+        break;
+      case 2:
+        ctxElement.setAttribute('id', '');
+        break;
     }
   }
 }
